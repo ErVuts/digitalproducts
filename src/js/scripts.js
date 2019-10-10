ba6d3c6d1b2b95e0a4e60c3f8f0722dded016859
ErVuts <ervuts@gmail.com>
2019-10-10 14:45:12 +0300

    fixed valid and img bugs

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -117,7 +117,7 @@ $(document).ready(function(){
 	  	let marker = new google.maps.Marker({
     	position: {lat: -7.930406, lng: 112.637736},
     	map: map,
-    	icon:'../img/pin.svg'
+    	icon:'../img/pin.png'
 		});
 	}
 
